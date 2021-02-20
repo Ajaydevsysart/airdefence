7a57fcb83dc11b4f90ad30cb1a4ed1d14f05c91f
ajaydevsysart <ajaydev@sysart.net>
2021-02-20 10:28:09 +0530

    final commit

--- air_defence/settings.py
+++ air_defence/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,6 +128,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'defence/static')
 ]
 STATIC_ROOT= os.path.join(BASE_DIR,'assets')
+STATIC_ROOT= os.path.join(BASE_DIR,'static')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+django_heroku.settings(locals())
 
